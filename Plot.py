4ffed48530e70c3792a4a7e76ab56861d69ba013
GianlucaPoliTo <gianluca.perna@hotmail.it>
2020-02-20 16:52:12 +0100

    Semi-final version

--- Plot.py
+++ Plot.py
@@ -3,25 +3,24 @@ import matplotlib
 from SciDAVis2Python import func_ale
 
 
-matplotlib.rcParams.update({'font.size': 30})
-plt.rcParams['font.family'] = 'serif'
-plt.rcParams['font.serif'] = 'Ubuntu'
-plt.rcParams['font.monospace'] = 'Ubuntu Mono'
-plt.rcParams['font.size'] = 22
-plt.rcParams['axes.labelsize'] = 20
-plt.rcParams['axes.labelweight'] = 'bold'
-plt.rcParams['axes.titlesize'] = 25
-plt.rcParams['xtick.labelsize'] = 20
-plt.rcParams['ytick.labelsize'] = 20
-plt.rcParams['legend.fontsize'] = 20
-plt.rcParams['figure.titlesize'] = 15
-plt.ioff()
-
-def plot_my(x_data, y_data, func, title, name_file, popt,fig_size = [16,9]):
+def draw (x_data, y_data, func, title, name_file, popt,fig_size = [16,9]):
+    matplotlib.rcParams.update({'font.size': 30})
+    plt.rcParams['font.family'] = 'serif'
+    plt.rcParams['font.serif'] = 'Ubuntu'
+    plt.rcParams['font.monospace'] = 'Ubuntu Mono'
+    plt.rcParams['font.size'] = 22
+    plt.rcParams['axes.labelsize'] = 20
+    plt.rcParams['axes.labelweight'] = 'bold'
+    plt.rcParams['axes.titlesize'] = 25
+    plt.rcParams['xtick.labelsize'] = 20
+    plt.rcParams['ytick.labelsize'] = 20
+    plt.rcParams['legend.fontsize'] = 20
+    plt.rcParams['figure.titlesize'] = 15
+    plt.ioff()
     plt.figure(figsize = fig_size)
     plt.plot(x_data, y_data, 'k.', label='data')
     plt.plot(x_data, func(x_data, *popt), 'r-',
-             label='fit: wn_x=%5.3f, wn_y=%5.3f, Eta_x=%5.3f, Eta_y=%5.3f, k_x=%5.3f, \
+             label='fit:\n wn_x=%5.3f\n, wn_y=%5.3f\n, Eta_x=%5.3f\n, Eta_y=%5.3f\n, k_x=%5.3f\n, \
              Eta_y=%5.3f' % tuple(popt), linewidth = 3)
     plt.xlabel('x')
     plt.ylabel('y')
--- SciDAVis2Python.py
+++ SciDAVis2Python.py
@@ -8,53 +8,51 @@ Created on Thu Feb 20 10:03:21 2020
 
 from scipy.optimize import curve_fit
 import numpy as np
-import matplotlib
 import argparse
 import pandas as pd
-from Plot import plot_my
+import Plot
 import os
-import random
 #Define the data to be fit with some noise:
 
 def func_ale (x, wn_x, wn_y, eta_x, eta_y, k_x, k_y):
+
     #la funzione è ok
     return np.sqrt(((1-(x/wn_x)**2)/(((1-(x/wn_x)**2)**2+eta_x**2)*k_x)+(-eta_y/(((1-(x/wn_y)**2)**2+eta_y**2)*k_y)))**2+((-eta_x/(((1-(x/wn_x)**2)**2+eta_x**2)*k_x))+(-(1-(x/wn_y)**2)/(((1-(x/wn_y)**2)**2+eta_y**2)*k_y)))**2)
 
 def set_p0_rule(x_value, parameters,tolerance):
     #usare x_data per settarli
-    wn_x = random.uniform(np.max(x_value) - tolerance/100*np.max(x_value), np.max(x_value) + tolerance/100*np.max(x_value))
-    wn_y = wn_x
-    #wn_y = random.uniform(np.max(y_data) - tolerance/100*np.max(y_data), np.max(y_data) + tolerance/100*np.max(y_data))
+    wn_x = np.max(x_value) - tolerance/100*np.max(x_value)
+    wn_y = np.max(x_value) + tolerance/100*np.max(x_value)
     Eta_x = parameters[0]
     Eta_y = parameters[1]
     K_x = parameters[2]
     K_y = parameters[3]
-    return np.array([wn_x, wn_y, Eta_x, Eta_y, K_x, K_y])
+    return np.array([wn_x, wn_y, Eta_x, Eta_y, K_x, K_y], dtype = float)
 
 def main(directory_p, parameters, tolerance):
-    #%%
     element = os.listdir(directory_p) #guardo tutti gli elementi nella directory
     #DA COMPLETARE LA RICERCA DEI FILE CSV OCCHIO ALLA VARIABILE I IN SAVEFIG
     #IMPLEMENTARE ANCHE IL SETTAGGIO DEI PARAMETRI INIZIALI
     for i in element:
         if i.split(".")[1] == "xls": #seleziono il primo file .csv
-            #FUNZIONA SOLO PRIMA COLONNA ORA
-            #colonne pari x colonne dispari y
+            folder_picture = os.path.join(directory_p,i.split(".")[1])
+            os.makedir(folder_picture)
             df = pd.read_excel(r"{}/{}".format(directory_p,i), encoding = "utf-8", header = 0)
             column = df.columns
-            col = 0
+            col = 1
             x_data = df.filter(regex="Frequenza")
             y_data = df.filter(regex="Ampiezza")
             for x,y in zip(x_data, y_data):
                 x_value = x_data[x].dropna()
                 y_value = y_data[y].dropna()
                 p0 = set_p0_rule(x_value, parameters, tolerance) #IMPOSTARE BENE!!
-                popt, pcov = curve_fit(func_ale, x_value, y_value, p0, method='lm')
-                plot_my (x, y, func_ale, "fit curve vs normal points", str(col)+".png", popt) #libreria nostra!
+                try:
+                    popt, pcov = curve_fit(func_ale, x_value, y_value, p0, method='lm', maxfev = 2000)
+                    Plot.draw(x_value, y_value, func_ale, "fit curve vs normal points", folder_picture + str(col)+".png", popt) #libreria nostra!
+                except:
+                    print ("{}, {} non converge".format(x,y))
+                    pass
                 col += 1
-            #i.split["."][0]+"_"+"???"+"png
-
-
 
 if __name__ == "__main__":
 
