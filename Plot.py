eb86ed5d65e05c8f57b036d4801c6ec7fdbc41f6
GianlucaPoliTo <gianluca.perna@hotmail.it>
2020-02-20 17:48:58 +0100

    Final

--- Plot.py
+++ Plot.py
@@ -5,8 +5,6 @@ from SciDAVis2Python import func_ale
 
 def draw (x_data, y_data, func, title, name_file, popt,fig_size = [16,9]):
     matplotlib.rcParams.update({'font.size': 30})
-    plt.rcParams['font.family'] = 'serif'
-    plt.rcParams['font.serif'] = 'Ubuntu'
     plt.rcParams['font.monospace'] = 'Ubuntu Mono'
     plt.rcParams['font.size'] = 22
     plt.rcParams['axes.labelsize'] = 20
@@ -20,8 +18,7 @@ def draw (x_data, y_data, func, title, name_file, popt,fig_size = [16,9]):
     plt.figure(figsize = fig_size)
     plt.plot(x_data, y_data, 'k.', label='data')
     plt.plot(x_data, func(x_data, *popt), 'r-',
-             label='fit:\n wn_x=%5.3f\n, wn_y=%5.3f\n, Eta_x=%5.3f\n, Eta_y=%5.3f\n, k_x=%5.3f\n, \
-             Eta_y=%5.3f' % tuple(popt), linewidth = 3)
+             label='fit:\n wn_x=%5.3f\n wn_y=%5.3f\n Eta_x=%5.3f\n Eta_y=%5.3f\n k_x=%5.3f\n k_y=%5.3f' % tuple(popt), linewidth = 3)
     plt.xlabel('x')
     plt.ylabel('y')
     plt.title(title)
--- SciDAVis2Python.py
+++ SciDAVis2Python.py
@@ -12,6 +12,7 @@ import argparse
 import pandas as pd
 import Plot
 import os
+
 #Define the data to be fit with some noise:
 
 def func_ale (x, wn_x, wn_y, eta_x, eta_y, k_x, k_y):
@@ -35,7 +36,7 @@ def main(directory_p, parameters, tolerance):
     #IMPLEMENTARE ANCHE IL SETTAGGIO DEI PARAMETRI INIZIALI
     for i in element:
         if i.split(".")[1] == "xls": #seleziono il primo file .csv
-            folder_picture = os.path.join(directory_p,i.split(".")[1])
+            folder_picture = os.path.join(directory_p,i.split(".")[0])
             os.mkdir(folder_picture)
             df = pd.read_excel(r"{}/{}".format(directory_p,i), encoding = "utf-8", header = 0)
             column = df.columns
@@ -48,7 +49,7 @@ def main(directory_p, parameters, tolerance):
                 p0 = set_p0_rule(x_value, parameters, tolerance) #IMPOSTARE BENE!!
                 try:
                     popt, pcov = curve_fit(func_ale, x_value, y_value, p0, method='lm', maxfev = 2000)
-                    Plot.draw(x_value, y_value, func_ale, "fit curve vs normal points", folder_picture + str(col)+".png", popt) #libreria nostra!
+                    Plot.draw(x_value, y_value, func_ale, "{} vs {}".format(x,y), os.path.join(folder_picture,str(col)+".png"), popt) #libreria nostra!
                 except:
                     print ("{}, {} non converge".format(x,y))
                     pass
@@ -63,5 +64,4 @@ if __name__ == "__main__":
     type = float)
     args = parser.parse_args()
     directory_p = args.directory
-    print ("{} {} {}".format(directory_p, args.parameters, args.tolerance))
     main(directory_p, args.parameters, args.tolerance)
