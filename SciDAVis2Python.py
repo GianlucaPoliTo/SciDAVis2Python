1b651a98d6c2dd8342c78354756979f9197ecd4d
rdimash <riccardo.dimarino@outlook.com>
2020-02-20 14:16:08 +0100

    scipy new ultime modifiche

--- SciDAVis2Python.py
+++ SciDAVis2Python.py
@@ -12,7 +12,7 @@ import matplotlib
 import argparse
 import pandas as pd
 from Plot import plot
-
+import os
 
 #Define the data to be fit with some noise:
 
@@ -31,24 +31,28 @@ def set_p0_rule(x_data, y_data):
     return np.array([wn_x, wn_y, Eta_x, Eta_y, K_x, K_y])
 
 def main(directory_p):
+    #%%
     element = os.listdir(directory_p) #guardo tutti gli elementi nella directory
     #DA COMPLETARE LA RICERCA DEI FILE CSV OCCHIO ALLA VARIABILE I IN SAVEFIG
     #IMPLEMENTARE ANCHE IL SETTAGGIO DEI PARAMETRI INIZIALI
     for i in element:
-        if i.split(".")[1] == "csv": #seleziono il primo file .csv
-            p0 = set_p0_rule #IMPOSTARE BENE!!
-
+        if i.split(".")[1] == "xls": #seleziono il primo file .csv
+        
             #FUNZIONA SOLO PRIMA COLONNA ORA
-            df = pd.read_excel(r"C:\Users\Gianl\Downloads\Telegram Desktop\Data.xls", encoding = "utf-8")  #colonne pari x colonne dispari y
-            column = df.column
+            #colonne pari x colonne dispari y
+            df = pd.read_excel(r"{}/{}".format(directory_p,i), encoding = "utf-8")
+            column = df.columns
 
-            x_data = df["Frequenza 1"]
-            y_data =df["Ampiezza 1"]
+            x_data = df.filter(regex="Frequenza")
+            y_data = df.filter(regex="Ampiezza")
 
             x_data = x_data.dropna()
             y_data = y_data.dropna()
-            popt, pcov = curve_fit(func_ale,x_data, y_data, p0 = p0, method='lm')
-            plot (????) #libreria nostra!
+            
+            p0 = set_p0_rule(x_data, y_data) #IMPOSTARE BENE!!
+            
+            popt, pcov = curve_fit(func_ale, x_data, y_data, p0, method='lm')
+            # plot (????) #libreria nostra!
             #i.split["."][0]+"_"+"???"+"png
 
 
